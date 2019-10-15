4eab8848384137abc9eb563989357d8a2c3dbf8f
igaurab <sushuoit@gmail.com>
2019-10-15 14:06:28 +0545

    WIP added location stream to GetLocation

--- example/lib/widgets/getLocation.dart
+++ example/lib/widgets/getLocation.dart
@@ -34,22 +34,50 @@ class GetLocation {
         latlng = LatLng(onValue.latitude, onValue.longitude);
         print(latlng);
       });
-
       return latlng;
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        var error = 'Permission denied';
+        print(error);
+      } else {
+        print('Uncaught Exception');
+      }
+      // currentLocation = null;
+    }
+    // return null;
+  }
 
-      // location.onLocationChanged().listen((LocationData currentLocation) {
-      //   if (currentLocation != null) {
-      //     LatLng latLng;
-      //     latLng.latitude = currentLocation.latitude;
-      //     latLng.longitude = currentLocation.longitude;
-      //     print(currentLocation.latitude);
-      //     print(currentLocation.longitude);
-      //     return latLng;
-      //   } else {
-      //     print('Current Location is null');
-      //     return null;
-      //   }
-      // });
+  Future<LatLng> getLocationStream() async {
+    var location = new Location();
+    var serviceStatus = await location.serviceEnabled();
+    print('Service Status $serviceStatus');
+
+    if (!serviceStatus) {
+      var val = await location.requestService();
+      print('Req service $val');
+      serviceStatus = await location.serviceEnabled();
+      print('Service Status after request $serviceStatus');
+      permission = await location.requestPermission();
+      print('Permission $permission');
+      if (!permission) {
+        val = await location.requestService();
+        print('Req service $val');
+      }
+    }
+    try {
+      await location.onLocationChanged().listen((LocationData currentLocation) {
+        if (currentLocation != null) {
+          LatLng latLng;
+          latLng.latitude = currentLocation.latitude;
+          latLng.longitude = currentLocation.longitude;
+          print(currentLocation.latitude);
+          print(currentLocation.longitude);
+          return latLng;
+        } else {
+          print('Current Location is null');
+          return LatLng(0, 0);
+        }
+      });
     } on PlatformException catch (e) {
       if (e.code == 'PERMISSION_DENIED') {
         var error = 'Permission denied';
--- example/lib/widgets/plugin_layer.dart
+++ example/lib/widgets/plugin_layer.dart
@@ -18,41 +18,52 @@ class MapsPluginLayer extends StatefulWidget {
 }
 
 class _MapsPluginLayerState extends State<MapsPluginLayer> {
+  MapController controller = MapController();
+
+  Future<LatLng> _latlng = loc.getLocationStream();
   @override
   Widget build(BuildContext context) {
     var customPlugin = MyCustomPluginOptions(context: context);
-    var latLng = customPlugin.getLocation();
-    // print("INSIDE PLUGIN: $latLng");
-
     return FutureBuilder(
-      future: customPlugin.getLocation(),
+      future: _latlng,
       initialData: LatLng(0, 0),
       builder: (context, AsyncSnapshot snapshot) {
         LatLng value = snapshot.data;
-        final _zoom = 10.0;
         print('Data of Snapshot ${value}');
-        print('${value.latitude}');
-        print('${value.longitude}');
+        var marker = [
+          Marker(
+              point: LatLng(value.latitude, value.longitude),
+              builder: (context) => Container(
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.redAccent,
+                    ),
+                  )),
+          Marker(
+              point: LatLng(37.6420767, -120.9977083),
+              builder: (context) => Container(
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.blueAccent,
+                    ),
+                  ))
+        ];
+        // Doing so, the map moves everytime the widget gets rebuild, which it happens every second
+        // We have to move the map only when if the current location is out of bound(screen)
+        // Same goes with the position of marker, we have to move the position of marker only if the data changes.
+        //For static data value, don't change the position of marker as well as map
 
-        widget.map.move(LatLng(value.latitude, value.longitude), _zoom);
-        // MapController mapController = MapController();
-        // mapController.move(LatLng(value.latitude, value.longitude), 10.0);
+        //Update: widget.map.zoom gives us the current zoom level of the map
+
+        widget.map
+            .move(LatLng(value.latitude, value.longitude), widget.map.zoom);
+
         return FlutterMap(
           options: MapOptions(
             center: LatLng(value.latitude, value.longitude),
           ),
           // mapController: controller,
-          layers: [
-            MarkerLayerOptions(markers: [
-              Marker(
-                  point: value,
-                  builder: (context) => Container(
-                        child: Icon(
-                          Icons.location_on,
-                        ),
-                      ))
-            ])
-          ],
+          layers: [MarkerLayerOptions(markers: marker)],
         );
       },
     );
