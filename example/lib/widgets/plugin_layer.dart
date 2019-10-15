9abbfdf145cf90791cf178d11acebfe8643605d1
igaurab <sushuoit@gmail.com>
2019-10-15 16:17:01 +0545

    WIP Marker

--- example/lib/widgets/plugin_layer.dart
+++ example/lib/widgets/plugin_layer.dart
@@ -20,34 +20,16 @@ class MapsPluginLayer extends StatefulWidget {
 class _MapsPluginLayerState extends State<MapsPluginLayer> {
   MapController controller = MapController();
 
-  Future<LatLng> _latlng = loc.getLocationStream();
   @override
   Widget build(BuildContext context) {
     var customPlugin = MyCustomPluginOptions(context: context);
     return FutureBuilder(
-      future: _latlng,
+      future: customPlugin.getLocation(),
       initialData: LatLng(0, 0),
       builder: (context, AsyncSnapshot snapshot) {
         LatLng value = snapshot.data;
         print('Data of Snapshot ${value}');
-        var marker = [
-          Marker(
-              point: LatLng(value.latitude, value.longitude),
-              builder: (context) => Container(
-                    child: Icon(
-                      Icons.location_on,
-                      color: Colors.redAccent,
-                    ),
-                  )),
-          Marker(
-              point: LatLng(37.6420767, -120.9977083),
-              builder: (context) => Container(
-                    child: Icon(
-                      Icons.location_on,
-                      color: Colors.blueAccent,
-                    ),
-                  ))
-        ];
+
         // Doing so, the map moves everytime the widget gets rebuild, which it happens every second
         // We have to move the map only when if the current location is out of bound(screen)
         // Same goes with the position of marker, we have to move the position of marker only if the data changes.
@@ -62,8 +44,22 @@ class _MapsPluginLayerState extends State<MapsPluginLayer> {
           options: MapOptions(
             center: LatLng(value.latitude, value.longitude),
           ),
-          // mapController: controller,
-          layers: [MarkerLayerOptions(markers: marker)],
+          layers: [
+            MarkerLayerOptions(markers: [
+              Marker(
+                  point: LatLng(value.latitude, value.longitude),
+                  builder: (context) {
+                    print("Marker is prinitng...");
+                    return Container(
+                        height: 30.0,
+                        width: 30.0,
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.redAccent,
+                        ));
+                  }),
+            ])
+          ],
         );
       },
     );
