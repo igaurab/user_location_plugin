48d482120c65e549a8a0787ebebbe776d720bb59
igaurab <sushuoit@gmail.com>
2019-10-17 14:53:49 +0545

    TileLayerOptions control given to the user

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,20 +22,37 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  MapController mapController = MapController();
+  List<Marker> markers = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Plugin User Location")),
         body: FlutterMap(
           options: MapOptions(
-            zoom: 20.0,
+            center: LatLng(27.7172, 85.3240),
+            zoom: 15.0,
             plugins: [
               UserLocationPlugin(),
             ],
           ),
           layers: [
-            UserLocationOptions(context: context),
+            TileLayerOptions(
+              urlTemplate: "https://api.tiles.mapbox.com/v4/"
+                  "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+              additionalOptions: {
+                'accessToken':
+                    'pk.eyJ1IjoiaWdhdXJhYiIsImEiOiJjazFhOWlkN2QwYzA5M2RyNWFvenYzOTV0In0.lzjuSBZC6LcOy_oRENLKCg',
+                'id': 'mapbox.streets',
+              },
+            ),
+            MarkerLayerOptions(markers: markers),
+            UserLocationOptions(
+                context: context,
+                mapController: mapController,
+                markers: markers),
           ],
+          mapController: mapController,
         ));
   }
 }
