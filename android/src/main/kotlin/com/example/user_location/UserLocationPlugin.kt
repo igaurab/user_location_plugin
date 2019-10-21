680185783e0b68a4430839f3c0bf4cf89729ec27
igaurab <sushuoit@gmail.com>
2019-10-21 17:24:58 +0545

    WIP Added locationStatusStream

--- android/src/main/kotlin/com/example/user_location/UserLocationPlugin.kt
+++ android/src/main/kotlin/com/example/user_location/UserLocationPlugin.kt
@@ -6,20 +6,57 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class UserLocationPlugin: MethodCallHandler {
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
+import io.flutter.plugin.common.EventChannel.StreamHandler
+import android.location.LocationListener
+import android.os.Bundle
+
+
+class UserLocationPlugin: MethodCallHandler, LocationListener {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
       val channel = MethodChannel(registrar.messenger(), "user_location")
       channel.setMethodCallHandler(UserLocationPlugin())
+
+      val eventChannel = EventChannel(registrar.messenger(),"locationPermissionStream")
+      eventChannel.setMethodCallHandler(UserLocationPlugin())
+
     }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
+    } 
+    else if(call.method == "onLocationStatusChanged" ) {
+        subscribeToLocationProvider(result)
+    }
+    else {
       result.notImplemented()
+    }
+  }
+
+  fun subscribeToLocationProvider(result: Result) {
+
+    val locationListenerGPS = object : LocationListener {
+      override fun onLocationChanged(location: android.location.Location) {
+        val latitude = location.latitude
+        val longitude = location.longitude
+      }
+
+      override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+
+      }
+
+      override fun onProviderEnabled(provider: String) {
+        result.success(true)
+      }
+
+      override fun onProviderDisabled(provider: String) {
+        result.success(false)
+      }
     }
   }
 }
