d0e459526761a5808dfff16b01f7db16c7a4ecb5
Murray Purves <murray@benali.local>
2019-11-28 21:13:00 +0000

    Adding check on first build

--- lib/src/user_location_layer.dart
+++ lib/src/user_location_layer.dart
@@ -30,62 +30,64 @@ class _MapsPluginLayerState extends State<MapsPluginLayer> {
   void _subscribeToLocationChanges() {
     var location = Location();
     location.requestService();
-    location.onLocationChanged().listen((onValue) {
-      _addsMarkerLocationToMarkerLocationStream(onValue);
-      setState(() {
-        if (onValue.latitude == null || onValue.longitude == null) {
-          _currentLocation = LatLng(0, 0);
-        } else {
-          _currentLocation = LatLng(onValue.latitude, onValue.longitude);
-          print(_currentLocation);
-        }
+    if (location.onLocationChanged() != null) {
+      location.onLocationChanged().listen((onValue) {
+        _addsMarkerLocationToMarkerLocationStream(onValue);
+        setState(() {
+          if (onValue.latitude == null || onValue.longitude == null) {
+            _currentLocation = LatLng(0, 0);
+          } else {
+            _currentLocation = LatLng(onValue.latitude, onValue.longitude);
+            print(_currentLocation);
+          }
 
-        var height = 20.0 * (1 - (onValue.accuracy / 100));
-        var width = 20.0 * (1 - (onValue.accuracy / 100));
-        print(" The size of accuracy marker is $height");
-        if (height < 0 || width < 0) {
-          height = 20;
-          width = 20;
-        }
+          var height = 20.0 * (1 - (onValue.accuracy / 100));
+          var width = 20.0 * (1 - (onValue.accuracy / 100));
+          print(" The size of accuracy marker is $height");
+          if (height < 0 || width < 0) {
+            height = 20;
+            width = 20;
+          }
 
-        widget.options.markers.clear();
-        widget.options.markers.add(Marker(
-            point:
-                LatLng(_currentLocation.latitude, _currentLocation.longitude),
-            builder: (context) {
-              return Stack(
-                alignment: AlignmentDirectional.center,
-                children: <Widget>[
-                  Container(
-                    height: height,
-                    width: width,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.blue[300].withOpacity(0.7)),
-                  ),
-                  widget.options.markerWidget ??
-                      Container(
-                        height: 10,
-                        width: 10,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle, color: Colors.blueAccent),
-                      )
-                ],
-              );
-            }));
+          widget.options.markers.clear();
+          widget.options.markers.add(Marker(
+              point:
+                  LatLng(_currentLocation.latitude, _currentLocation.longitude),
+              builder: (context) {
+                return Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: <Widget>[
+                    Container(
+                      height: height,
+                      width: width,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue[300].withOpacity(0.7)),
+                    ),
+                    widget.options.markerWidget ??
+                        Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.blueAccent),
+                        )
+                  ],
+                );
+              }));
 
-        if (widget.options.updateMapLocationOnPositionChange &&
-            widget.options.mapController != null) {
-          widget.options.mapController.move(
-              LatLng(_currentLocation.latitude ?? LatLng(0, 0),
-                  _currentLocation.longitude ?? LatLng(0, 0)),
-              widget.map.zoom ?? 15);
-        } else if (widget.options.updateMapLocationOnPositionChange) {
-          print(
-              "Warning: updateMapLocationOnPositionChange set to true, but no mapController provided: can't move map");
-        }
+          if (widget.options.updateMapLocationOnPositionChange &&
+              widget.options.mapController != null) {
+            widget.options.mapController.move(
+                LatLng(_currentLocation.latitude ?? LatLng(0, 0),
+                    _currentLocation.longitude ?? LatLng(0, 0)),
+                widget.map.zoom ?? 15);
+          } else if (widget.options.updateMapLocationOnPositionChange) {
+            print(
+                "Warning: updateMapLocationOnPositionChange set to true, but no mapController provided: can't move map");
+          }
+        });
       });
-    });
+    }
   }
 
   _addsMarkerLocationToMarkerLocationStream(LocationData onValue) {
