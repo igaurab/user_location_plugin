e9569ca1691a8ee8fd0db3442afb16a07dc70ea1
Ivan Saprykin <kartoshka95@gmail.com>
2020-01-21 18:14:56 +0500

    adds "locationStatusStream" in flutter event channel iOS module.(#27)
    * init ios
    
    * Delete Flutter.podspec
    
    * fix
    
    * Add Swift version

--- ios/Classes/UserLocationPlugin.m
+++ ios/Classes/UserLocationPlugin.m
@@ -1,5 +1,9 @@
 #import "UserLocationPlugin.h"
+#if __has_include(<user_location/user_location-Swift.h>)
 #import <user_location/user_location-Swift.h>
+#else
+#import "user_location-Swift.h"
+#endif
 
 @implementation UserLocationPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
